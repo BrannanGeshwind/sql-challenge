34003f8fb5a4814bce870fe38fe0479919327730
BrannanGeshwind <brannan.geshwind@gmail.com>
2023-11-29 23:01:20 -0500

    Pushing final code to repo.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # sql-challenge
 Repo for Module 9 Challenge (SQL)
+
+8/16 Updated repo with SQL code for creating tables, imported CSV files. Used QuickDBD to create ERD, added to folder as well. Updated to pgAdmin 4.7, connection errors resolved.
+
+
+
+
+
+RESOURCES USED:
+https://www.quickdatabasediagrams.com/
+https://stackoverflow.com/
+tutorialspoint.com/index.htm
--- employee_sql.sql
+++ employee_sql.sql
@@ -6,33 +6,51 @@ DROP TABLE employees;
 DROP TABLE salaries;
 DROP TABLE titles;
 
+--Verifying tables made and data imported
+SELECT *
+FROM --table name--
+LIMIT 20;
+
 --Creating needed tables for importing csvs
 CREATE TABLE departments (
-	dept_no VARCHAR,
-	dept_name VARCHAR
+	dept_no VARCHAR(300) PRIMARY KEY,
+	dept_name VARCHAR(300) NOT NULL
 );
-CREATE TABLE dept_emp (
-	emp_no INT,
-	dept_no VARCHAR
-);
-CREATE TABLE dept_manager (
-	dept_no VARCHAR,
-	emp_no INT
-);
-CREATE TABLE employees (
-	emp_no INT,
-	emp_title_id VARCHAR,
-	birth_date DATE,
-	first_name VARCHAR,
-	last_name VARCHAR,
-	sex VARCHAR,
-	hire_date DATE
-);
-CREATE TABLE salaries (
-	emp_no INT,
-	salary INT
-);
+
 CREATE TABLE titles (
-	title_id VARCHAR,
-	title VARCHAR
+	title_id VARCHAR(300) PRIMARY KEY,
+	title VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE employees (
+	emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR(300) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(300) NOT NULL,
+	last_name VARCHAR(300) NOT NULL,
+	sex VARCHAR(300) NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(300),
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(300),
+	emp_no INT,
+	PRIMARY KEY (dept_no, emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+CREATE TABLE salaries (
+	emp_no INT PRIMARY KEY,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
